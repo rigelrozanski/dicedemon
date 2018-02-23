02fcdf40e827e1eba2e4f66b15c1307b37fd1933
rigelrozanski <rigel.rozanski@gmail.com>
2018-02-23 10:47:43 +0000

    werkin program

--- src/main.rs
+++ src/main.rs
@@ -28,9 +28,9 @@ struct Opt {
 }
 
 fn main() {
-    
+  
+    // get cli input
     let opt = Opt::from_args();
-    println!("{:?}", opt);
    
     // set num words
     let numwords = opt.numwords;
@@ -49,7 +49,13 @@ fn main() {
 
     let mut buffer = String::new();
     let mut wordvec: Vec<String> = vec![];
-   
+    
+    let lang = Language::English;
+    let word_map = lang.get_wordmap(); 
+    let word_list = lang.get_wordlist();
+
+    // Retrieve the words
+    println!("Enter word the first {} words then the checksum word will be computed.", (numwords -1));
     for i in 0..(numwords-1) {
         if opt.debug {
             let debug_word = "abandon";
@@ -57,25 +63,33 @@ fn main() {
             continue;
         }
 
-        print!("Enter word {}: ", (i+1));
+        print!("{}.\t",(i+1));
 
         let _=stdout().flush();
         stdin().read_line(&mut buffer).expect("Did not enter a correct string");    
+        buffer.pop(); // remove the trailing new line
+
+        let _ = match word_map.get(&buffer) {
+            Some(_) => {},
+            None => {
+                println!("Entered non-bip39 word! ({})", &buffer);
+                return;
+            }
+        };
+        
         wordvec.push(buffer.clone());
         buffer.clear();
     }
 
-    assert_eq!(numwords-1,target_mnemonic.word_count()-1);
+    assert_eq!(numwords, target_mnemonic.word_count());
+
+    // get word bits from entered words
     let added_entropy= target_mnemonic.entropy_bits() - (numwords-1) *11;     
-    let lang = Language::English;
-    let word_map = lang.get_wordmap(); 
-    let word_list = lang.get_wordlist();
     let mut to_validate: BitVec = BitVec::new();
-    
     for word in wordvec {
         let n = match word_map.get(&word) {
             Some(n) => n,
-            None => panic!("uh oh")
+            None => panic!("bad non-bip39 made it through!")
         };
         for i in 0..11 {
             let bit = bit_from_u16_as_u11(*n, i);
@@ -83,32 +97,29 @@ fn main() {
         }
     }
 
-
+    // create the entropy (all 0s) - TODO add custom entropy bits to CLI
     for _ in 0..added_entropy{
         let bit = false; //used fixed bits
         to_validate.push(bit);
     }
-
     let entropy = to_validate.to_bytes();
     let hash = sha256(entropy.as_ref());
-
     let entropy_hash_to_validate_bits = BitVec::from_bytes(hash.as_ref());
-
+    
+    // add entropy bytes to the word bits
     &to_validate.extend(entropy_hash_to_validate_bits.into_iter().take(target_mnemonic.checksum_bits()));
-
     let word_bytes = &to_validate.to_bytes();
     let mut reader = BitReader::new(word_bytes);
 
+    // read all the words back out (TODO only read the final word out?)
     let mut words: Vec<&str> = Vec::new();
     for _ in 0..numwords {
         let n = reader.read_u16(11);
         words.push(word_list[n.unwrap() as usize].as_ref());
     }
 
-    // print the output
-    for x in 0..numwords {
-        println!("{}.\t{}",x, words[x])
-    }
+    // print the final checksum word
+    println!("{}.\t{}",numwords, words[numwords-1])
 }
 
 fn sha256(input: &[u8]) -> Vec<u8> {
@@ -120,7 +131,5 @@ fn sha256(input: &[u8]) -> Vec<u8> {
 fn bit_from_u16_as_u11(input: u16, position: u16) -> bool {
     if position < 11 {
         input & (1 << (10 - position)) != 0
-    } else {
-        false
-    }
+    } else { false }
 }
